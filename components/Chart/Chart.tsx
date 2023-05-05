58723a5e35528d7c92db4272bc10876ceafca0cf
Yeaeun Han <yenhan.dev@gmail.com>
2023-05-05 15:06:17 -0400

    feat: add label to pie chart

--- components/Chart/Chart.tsx
+++ components/Chart/Chart.tsx
@@ -9,6 +9,12 @@ function Chart() {
         { title: "Two", value: 15, color: "#C13C37" },
         { title: "Three", value: 20, color: "#6A2135" },
       ]}
+      label={(data) => data.dataEntry.title}
+      labelPosition={50}
+      labelStyle={{
+        fontSize: ".8rem",
+        fill: "#fff",
+      }}
     />
   );
 }
