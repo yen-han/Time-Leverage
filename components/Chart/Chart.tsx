4c9223a109726418b0a16c2f4ce00d489309dd34
Yeaeun Han <yenhan.dev@gmail.com>
2023-05-04 21:09:31 -0400

    feat: add pie chart

---
+++ components/Chart/Chart.tsx
@@ -0,0 +1,15 @@
+import { PieChart } from "react-minimal-pie-chart";
+function Chart() {
+  return (
+    <PieChart
+      animate={true}
+      animationDuration={300}
+      data={[
+        { title: "One", value: 10, color: "#E38627" },
+        { title: "Two", value: 15, color: "#C13C37" },
+        { title: "Three", value: 20, color: "#6A2135" },
+      ]}
+    />
+  );
+}
+export default Chart;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.scss";
 import Menu from "@/components/Menu/Menu";
+import Chart from "@/components/Chart/Chart";
 
 export default function Home() {
   return (
@@ -14,6 +15,14 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} bg-warning bg-opacity-10`}>
         <Menu />
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-sm-6 col-xs-12">
+              <Chart />
+            </div>
+            <div className="col-sm-6 col-xs-12"></div>
+          </div>
+        </div>
       </main>
     </>
   );
