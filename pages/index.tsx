2312ecd5aac49a930807cfad4d21035e542c376b
Yeaeun Han <yenhan.dev@gmail.com>
2023-05-11 16:16:40 -0400

    feat: add datepicker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
+import React, { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.scss";
 import Menu from "@/components/Menu/Menu";
 import Chart from "@/components/Chart/Chart";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function Home() {
+  const [selectDate, setSelectDate] = useState(new Date());
   return (
     <>
       <Head>
@@ -13,14 +17,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} bg-warning bg-opacity-10`}>
+      <main className={`${styles.Home} bg-warning bg-opacity-10`}>
         <Menu />
         <div className="container mt-5 px-5">
           <div className="row">
             <div className="col-sm-6 col-xs-12">
               <Chart />
             </div>
-            <div className="col-sm-6 col-xs-12"></div>
+            <div className={`col-sm-6 col-xs-12`}>
+              <DatePicker
+                className={`${styles.datepicker}`}
+                selected={selectDate}
+                onChange={(date) => {
+                  if (date) setSelectDate(date);
+                }}
+              />
+            </div>
           </div>
         </div>
       </main>
