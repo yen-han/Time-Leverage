d8a8b053f6594f3d8ca9c3b506eaedb4e63d7190
Yeaeun Han <yenhan.dev@gmail.com>
2023-05-14 13:40:30 -0400

    feat: add tag component

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu/Menu";
+import Tag from "@/components/Tag/Tag";
 import styles from "./tags.module.scss";
 export default function Tags() {
   return (
@@ -8,7 +9,8 @@ export default function Tags() {
         <div className="container mt-5 px-5 px-sm-2">
           <div className="row">
             <div className="col-sm-6 col-xs-12">
-              <h1>Tags</h1>
+              <h1 className="mb-4">Tags</h1>
+              <Tag />
             </div>
             <div className={`col-sm-6 col-xs-12`}></div>
           </div>
