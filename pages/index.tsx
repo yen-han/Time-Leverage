57f3d1a54c304ab14fc2d40db0eedef20ae11fbe
Yeaeun Han <yenhan.dev@gmail.com>
2023-09-24 21:44:47 -0400

    make svg hidden

--- pages/index.tsx
+++ pages/index.tsx
@@ -102,6 +102,7 @@ export default function Home() {
                     fill="currentColor"
                     className={`bi bi-plus ${styles.newTimeIcon}`}
                     viewBox="0 0 16 16"
+                    aria-hidden="true"
                   >
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
                   </svg>
