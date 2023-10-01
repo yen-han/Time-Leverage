05f3b69e4a8d2e0b30c43fcc55f2cde253018879
Yeaeun Han <yenhan.dev@gmail.com>
2023-09-30 22:28:44 -0400

    make tag name autofocus

--- components/EditTag/EditTag.tsx
+++ components/EditTag/EditTag.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "./editTag.module.scss";
 import IconPicker from "@/components/IconPicker/IconPicker";
 import axios from "axios";
@@ -16,8 +16,12 @@ function EditTag({ type, props }: Props) {
     class: "bi-emoji-smile-fill",
     path: "M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zM4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8z",
   });
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
     if (type == "edit") {
       setTitle(props.tag.title);
       setColor(props.tag.bgColor);
@@ -82,6 +86,7 @@ function EditTag({ type, props }: Props) {
             onChange={(e) => {
               setTitle(e.target.value);
             }}
+            ref={inputRef}
           />
           <div id="tagFeedback" className="invalid-feedback">
             Please enter tag name.
